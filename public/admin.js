e8c643569ee6457cef324243bf6b72895b419d2c
Jose Covarrubias <Emilianocovarrubias5@gmail.com>
2023-02-11 19:34:09 -0800

    add

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,45 @@
 
 // Your Code Here
+async function main() {
+
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+
+    books.forEach(renderBook)
+}
+
+function renderBook(book) {
+
+    let root= document.getElementById("root")
+    let listItem= document.createElement ("li")
+    let qtyInput = docuemnt.createElement("input")
+    let saveBtn = document.createElement("button")
+
+
+
+    listItem.innerHTML= `${book.title}`
+    qtyInput.setAttribute('value', `${book.quantity}`)
+    saveBtn.textContent= "save "
+
+    saveBtn.addEventListener("list", () => {
+            fetch('http://localhost:3001/updateBook', {
+                method: "PATCH",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: book.id,
+                    quantity: qtyInput.value
+                })
+            })
+        }) 
+
+   
+    listItem.append(qtyInput, saveBtn)
+    root.append(listItem)
+
+
+
+}
+
+main()
