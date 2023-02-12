2850dd479252da38dc12c3992a26040a4aa1371d
Stanley Jackson <stanleyjackson11.sj@gmail.com>
2023-02-12 07:46:17 -0800

    Code Added to admin.js

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,40 @@
+async function main() {
+
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+
+    books.forEach(renderBook)
+}
 
 // Your Code Here
+function renderBook(book){
+
+    let root = document.getElementById("root")
+    let listItem = document.createElement("li")
+    let qtyInput = document.createElement("input")
+    let saveBtn = document.createElement("button")
+    
+    qtyInput.setAttribute(value , `${book.quantity}`)
+    listItem.innerHTML = `${book.tittle}`
+    saveBtn.textContent = "Save"
+
+    saveBtn.addEventListener('click', () => {
+    fetch('http://localhost:3001/updateBook',{
+    method: "PATCH",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        id: book.id,
+        quantity: qtyInput.value
+    })
+})
+})
+
+    listItem.append(qtyInput, saveBtn)
+    root.append(listItem)
+
+
+}
+
+main()
